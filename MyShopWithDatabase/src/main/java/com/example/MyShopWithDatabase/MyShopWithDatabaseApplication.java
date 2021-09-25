3ea213d248d6e735e3623f97099d6b7c7ee49029
Marcin Szewczyk <marcinszewczyk.arch@gmail.com>
2021-09-25 11:41:27 +0200

    Initial commit

---
+++ MyShopWithDatabase/src/main/java/com/example/MyShopWithDatabase/MyShopWithDatabaseApplication.java
@@ -0,0 +1,13 @@
+package com.example.MyShopWithDatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyShopWithDatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyShopWithDatabaseApplication.class, args);
+	}
+
+}
---
+++ MyShopWithDatabase/src/test/java/com/example/MyShopWithDatabase/MyShopWithDatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MyShopWithDatabase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyShopWithDatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
